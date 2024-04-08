e1f2cdaf2bdb43a1d507e61d25b24f814d61fd4c
Suryakant Singh <suryakant12m@gmail.com>
2024-04-08 02:51:32 -0700

    first commit

---
+++ src/main/java/org/example/GiphyViewsTrackerBot.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class GiphyViewsTrackerBot {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String giphyProjectUrl = "https://giphy.com/gifs/i3q36zEFShC18oS6v0";
+        String telegramBotToken = "6768561787:AAH7oNzeB4DEPqclN91HPnf2nJlwOCvHwa8";
+        String chatId = "5930317471";
+
+        driver.get(giphyProjectUrl);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement viewsElement = driver.findElement(By.xpath("//div[@ class=\"ViewCountContainer-sc-15ri43l hCQudc ss-view\"]"));
+
+        String totalViews = viewsElement.getText();
+        System.out.println(totalViews);
+
+        //driver.quit();
+
+
+        sendTelegramMessage(telegramBotToken, chatId, "Daily Giphy Views Update:Total Views: " + totalViews);
+    }
+
+
+    public static void sendTelegramMessage(String botToken, String chatId, String message) {
+        String urlString = "https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + chatId + "&text=" + message;
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.getResponseCode();
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
